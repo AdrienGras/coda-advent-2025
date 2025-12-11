6b91e70cd21362039b9748c7b46e26044fd9c5f9
Adrien Gras <agr@owlnext.fr>
2025-12-11 12:23:50 +0100

    jour 11 v2

--- jour-11/src/Navigation/Building.php
+++ jour-11/src/Navigation/Building.php
@@ -34,12 +34,21 @@ class Building
         // Start at ground floor (0)
         $result = 0;
 
-        // Split the signal stream into individual characters
-        $streamAsArray = str_split($signalStream);
+        // Using strlen for accurate length calculation
+        $length = \strlen($signalStream);
 
         // Process each character and accumulate floor changes
-        foreach ($streamAsArray as $c) {
-            $result += $values[$c] ?? 0;  // Add value or 0 if character not recognized
+        // missing my FP accumulators here... :'(
+        $iterator = 0;
+        for ($iterator = 0; $iterator < $length; $iterator++) {
+            // Using array access for character retrieval
+            // This is more efficient than mb_substr for single character access
+            // and more efficient than str_split which creates an array of all characters
+            $char = $signalStream[$iterator];
+
+            // Accumulate floor changes based on character and scoring rules
+            // If character is not in values, default to 0 (ignore it, null accumulation)
+            $result += $values[$char] ?? 0;
         }
 
         return $result;
